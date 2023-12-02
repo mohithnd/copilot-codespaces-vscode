9023d91de218c51bbb7064eda244d75f48e95a93
Mohit Agrawal <108994458+mohithnd@users.noreply.github.com>
2023-12-02 14:57:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = [
+        { name: "HTML", level: 8 },
+        { name: "CSS", level: 7 },
+        { name: "JS", level: 6 },
+        { name: "React", level: 5 },
+    ];
+    return skills;
+}
